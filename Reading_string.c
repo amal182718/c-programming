ea580b444f1e6cf3e325bd7dfe33eadca00ffdc4
amal182718 <mathewsrejeevamal@gmail.com>
2026-09-08 12:59:23 +0530

    Create Reading_string.c

---
+++ Reading_string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  printf("size of integer=%ld",sizeof(int));
+  printf("\nsize of long integer=%ld",sizeof(long int));
+  printf("\nsize of short integer=%ld",sizeof(short int));
+  printf("\nsize of float integer=%ld",sizeof(float));
+  printf("\nsize of double integer=%ld",sizeof(double));
+  printf("\nsize of char integer=%ld",sizeof(char));
+  return 0;
+}
