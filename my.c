2c39b1576ab9f0caffa6ac77dcc6af605c959d1d
amal182718 <mathewsrejeevamal@gmail.com>
2026-09-08 12:49:48 +0530

    Create my.c

---
+++ my.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  char value;
+  printf("\nEnter a charac in capital letter:");
+  value=getchar();
+  value=value+32;
+  printf("Lower case letter: %c\n",value);
+  
+  return 0;
+}
